dc82ca35d51ce3110aa531ebbe508f574b603c9b
Saif-711 <saifnairat23@gmail.com>
2026-03-22 16:14:11 +0000

    create blog entity

---
+++ System/System/src/main/java/blogging/System/Model/BlogPost.java
@@ -0,0 +1,34 @@
+package blogging.System.Model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "blogs")
+public class BlogPost {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Title shouldn't be Empty")
+    private String title;
+    @NotBlank(message = "Content shouldn't be Empty")
+    private String content;
+
+    @NotBlank(message = "Category shouldn't be Empty")
+    private String Category;
+    @NotNull(message = "createdAt is required")
+    private LocalDateTime createdAt=LocalDateTime.now();
+    private LocalDateTime updatedAt=LocalDateTime.now();
+}
